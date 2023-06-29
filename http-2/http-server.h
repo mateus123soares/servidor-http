5ee014a81c37e5e0164546536081214c45def099
Mateus Soares <mateus123soares@hotmail.com>
2023-06-29 14:18:03 -0300

    versao final

--- http-2/http-server.h
+++ http-2/http-server.h
@@ -1,6 +1,6 @@
 #define MAX_CONNECTIONS 5
 #define MESSAGE_SIZE 2048
-#define HTTP_PORT 50002
+#define HTTP_PORT 50001
 #define MAX_FILE_BUFF 2136038
 #define SERVER_NAME "server-redes"
 #define MAX_CLIENTES 100
@@ -10,7 +10,7 @@
 #include <semaphore.h>
 
 struct sockaddr_in caddr, saddr;
-int total_taxa_server;
+int total_taxa_server,max_vazao_server;
 sem_t semaforo;
 
 int initServer(struct sockaddr_in caddr,struct sockaddr_in saddr);
--- http-2/server.c
+++ http-2/server.c
@@ -32,26 +32,28 @@ int main(int argc, char const* argv[]) {
     saddr.sin_port = htons(HTTP_PORT);
 
     sem_init(&semaforo, 0, 1);
-    int csize  = sizeof caddr;
-    int max_vazao_server = atoi(argv[1]);
+    max_vazao_server = atoi(argv[1]);
     printf("Vazao Maxima do Server: %d \n",max_vazao_server);
+
+    int csize  = sizeof caddr;
     int server_connection = initServer(caddr,saddr);
     
     while (1) {
-        if (total_taxa_server < max_vazao_server){
+  
             int client_socket_connection;
             pthread_t thread_id;
 
+            while((max_vazao_server - total_taxa_server) < 0) {
+                printf("O servidor atingiu o maximo de vazão");
+                printf("aguardando novas conexões\n");
+                sleep(2);
+            }
+
             // Aceitar uma conexão de cliente
             client_socket_connection = accept(server_connection, (struct sockaddr *)&caddr, &csize);
-            printf("Cliente conectado: %d",client_socket_connection);
             // Criar uma nova thread para atender o cliente
             pthread_create(&thread_id, NULL, RequestFileExtensionParse, (void *)&client_socket_connection);
-        }
-        else {
-             printf("O servidor atingiu o maximo de vazão");
-             break;
-        }
+         
     }
     
     // Fechar o socket do servidor
@@ -67,12 +69,18 @@ void *RequestFileExtensionParse(void* client_socket){
 	read(socket_cli, buffer, BUFFER_SIZE);
     // Realizando o parse da requisição realizada do usuário: METODO ARQUIVO EXTENÇÃO 
     method_http_request = strtok(buffer, " \t\n");
-
     file_path_http_request = strtok(NULL, " \t");
-    file_extension_request = strtok(file_path_http_request,".");
-    file_extension_request = strtok(NULL, " \t");
+    if(strcmp(file_path_http_request,"/")!=0){
+        file_extension_request = strtok(file_path_http_request,".");
+        file_extension_request = strtok(NULL, " \t");
+    }
+    else {
+        file_path_http_request = "/index";
+        file_extension_request = ".";
+        file_extension_request = "html";
+    }
 
-    printf("%d %s %s %s \n",socket_cli,method_http_request,file_path_http_request,file_extension_request);
+    printf("Client ID: %d VERBO %s PATH %s EXT %s \n",socket_cli,method_http_request,file_path_http_request,file_extension_request);
     RequestFindFile(socket_cli,method_http_request,file_path_http_request,file_extension_request);
 }
 
@@ -92,15 +100,9 @@ void * RequestFindFile(int cli_socket, char *method_http_request, char *file_pat
     GetCurrentDir(current_dir, FILENAME_MAX);
     strcpy(file_dir,current_dir);
     strcat(file_dir,"/htdocs");
-
-    if(strcmp(file_path_http_request,"/")==0){
-        strcat(file_dir,"/index.html");
-    }
-    else{
-        strcat(file_dir,file_path_http_request);
-        strcat(file_dir,".");
-        strcat(file_dir,file_extension_request);
-    }
+    strcat(file_dir,file_path_http_request);
+    strcat(file_dir,".");
+    strcat(file_dir,file_extension_request);
 
     fp = fopen(file_dir, "r");
     if (fp != NULL) {
@@ -118,16 +120,20 @@ void * RequestFindFile(int cli_socket, char *method_http_request, char *file_pat
             write(cli_socket, buffer, bytes_read);
             gettimeofday(&timeval2, NULL);
             close(cli_socket);  
-            double rtt_html = (double) (timeval2.tv_usec - timeval1.tv_usec) / 1000000 + (double) (timeval2.tv_sec - timeval1.tv_sec);
-            printf("RTT de envio do HTML: %fs \n",rtt_html);      
+            double rtt_html = (timeval2.tv_sec - timeval1.tv_sec) * 1000000 + (timeval2.tv_usec - timeval1.tv_usec);
+            printf("RTT de envio do HTML: %fms \n",rtt_html);      
         }else{
             printf("Endereço IP do cliente: %s\n", clientIP);
             // Aguarda permissão para acessar a variável "taxa"
             sem_wait(&semaforo);
             TAXA_MAX=checkIPExistence(clientIP);
-            total_taxa_server = total_taxa_server + 1000;
+            total_taxa_server = total_taxa_server + TAXA_MAX;
             // Libera o semáforo para permitir que outra thread acesse a variável
             sem_post(&semaforo);
+            while(((max_vazao_server - total_taxa_server)) < 0 ) {
+                printf("Aguardando\n");
+                sleep(1);
+            }
             printf("Taxa: %d \n",total_taxa_server);
             char max_taxa[TAXA_MAX];
             sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: image/%s\r\nContent-Length: %ld\r\n\r\n", file_extension_request,bytes_read);
@@ -147,12 +153,12 @@ void * RequestFindFile(int cli_socket, char *method_http_request, char *file_pat
             printf("Taxa: %d \n",total_taxa_server);
 
             close(cli_socket);  
-            double rtt_img = (timeval2.tv_sec - timeval1.tv_sec) * 1000000 + (timeval2.tv_usec - timeval1.tv_usec);
+            double rtt_html = (timeval2.tv_sec - timeval1.tv_sec) * 1000000 + (timeval2.tv_usec - timeval1.tv_usec);
             printf("Total Bytes send: %d \n",totalBytesWritten);
-            double largura_de_banda =  calculateTransferRate(totalBytesWritten,rtt_img);
+            double largura_de_banda =  calculateTransferRate(totalBytesWritten,rtt_html);
             
 
-            printf("Atraso fim-a-fim: %.2f ms\n", rtt_img);
+            printf("Atraso fim-a-fim: %.2f ms\n", rtt_html);
             printf("Largura da banda: %.2f kbps \n",largura_de_banda); 
             printf("Taxa Utilizada: %ld\n", sizeof(max_taxa));
         }
@@ -161,6 +167,7 @@ void * RequestFindFile(int cli_socket, char *method_http_request, char *file_pat
         write(cli_socket, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body>404 File Not Found</body></html>", strlen("HTTP/1.0 404 Not Found\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body>404 File Not Found</body></html>"));
         close(cli_socket);
     }
+    fclose(fp);
 }
 
 char* GetFileSize(char *file_dir, long* bytes_read){
